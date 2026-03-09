163ffa485e5632f00654fda006d4917d9c339b87
Clement Bobin <119869686+ClementBobin@users.noreply.github.com>
2026-03-09 14:36:16 +0100

    Change layout of hobbies section in Sidebar component

--- src/components/Resume/Sidebar.tsx
+++ src/components/Resume/Sidebar.tsx
@@ -161,7 +161,7 @@ export function Sidebar({ config = resumeConfig }: SidebarProps) {
       {/* Centres d'intérêt */}
       {hobbies && hobbies.length > 0 && labels.sections.hobbies && (
         <SidebarSection title={resolve(labels.sections.hobbies)}>
-          <div className="grid grid-cols-1 sm:grid-cols-2 gap-3">
+          <div className="flex flex-col gap-3">
             {(visibleHobbies ?? []).map((hobby, i) => (
               <div
                 key={`${resolve(hobby.title)}-${i}`}
